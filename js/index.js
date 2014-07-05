e571ed89a36930a5cbb8617edd33f8eb9c84de58
deefactorial <deefactorial@gmail.com>
2014-07-05 11:42:52 -0700

    Revert "Text Format"
    This reverts commit 6bcab0c74c85122156eefe2e60d13f4bb0f84e57.

--- js/index.js
+++ js/index.js
@@ -144,8 +144,8 @@ function setLoginLogoutButton() {
 	if (!config.user) {
 		$( ".todo-login" ).show().click( function() {
 			doFirstLogin( function(error) {
-				//If re-logging in tiggerSync will through timeout error.
-	        	//TODO: Stop triggerSync on Logout.
+				//If re-logging in tiggerSync will through timeout error
+	        	//TODO: stop triggerSync on Logout.
 				if (error && error != "timeout") { return loginErr( error ) }
 				goIndex()
 			} )
@@ -297,8 +297,8 @@ The sharing and login management stuff
 function doShare(id) {
     if (!config.user) {
         doFirstLogin(function(err) {
-        	//If re-logging in tiggerSync will through timeout error.
-        	//TODO: Stop triggerSync on Logout
+        	//If re-logging in tiggerSync will through timeout error
+        	//TODO: stop triggerSync on Logout
             if (err && err != "timeout") {
                 return loginErr(err)
             }
