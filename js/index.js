7bc90d95baf7dd3a8e1ba39f9aa3987e662032cb
deefactorial <deefactorial@gmail.com>
2014-07-20 14:34:35 -0700

    set user upon login through register server

--- js/index.js
+++ js/index.js
@@ -535,7 +535,16 @@ function doServerLogout( callBack ) {
 
 function registerServer(callBack) {
 	log( "Resister Server SessionID" )
-	doServerLogin( callBack )
+	doServerLogin( function(error, data) {
+		if (error) { return callBack( error ) }
+		config.setUser( data, function(error, ok) {
+			if (error) { return callBack( error ) }
+			config.syncReference = triggerSync( function(error, ok) {
+				log( "triggerSync done " + JSON.stringify( error ) )
+				callBack( error, ok )
+			} )
+		} )
+	} )
 }
 
 function registerFacebookToken(cb) {
