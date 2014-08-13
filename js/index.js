f830e31b160f9b8adbbc38fecdcb22a99a88fb91
Chris Anderson <jchris@couchbase.com>
2014-08-13 14:35:24 -0700

    avoid android thing on iOS

--- js/index.js
+++ js/index.js
@@ -582,10 +582,14 @@ function setupConfig(done) {
         console.log("getURL: " + JSON.stringify([err, url]))
         if (err) {return done(err)}
 
-        var xmlHttp = new XMLHttpRequest()
-        xmlHttp.open( 'GET', url, false )
-        xmlHttp.send( null )
-        console.log( 'XMLHttpRequest get: ' +  xmlHttp.responseText )
+        if (!/Apple/.test(navigator.userAgent)) {
+            // this helps on Android < 4.4
+            // otherwise basic auth doesn't work
+            var xmlHttp = new XMLHttpRequest()
+            xmlHttp.open( 'GET', url, false )
+            xmlHttp.send( null )
+            console.log( 'XMLHttpRequest get: ' +  xmlHttp.responseText )
+        }
 
         window.server = coax(url);
         var db = coax([url, appDbName]);
